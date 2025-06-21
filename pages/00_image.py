96b70aa20526d3a228c77ff05a8bbb4380443799
keriaker <cs2@daecheong.ms.kr>
2025-06-21 13:10:05 +0900

    Create 00_image.py

---
+++ pages/00_image.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.image("https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExOWh5emlyMGFrY2pxZjR0ZDA0bnJpNWh6cmo0Y2Zobzg2cTh1Y3FweSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/14d8Aj81ffgnQY/giphy.gif")
