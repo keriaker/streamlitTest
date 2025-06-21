4ea5f26154a654b24eb4e771f71880b9f3e1fd1f
keriaker <cs2@daecheong.ms.kr>
2025-06-21 13:18:57 +0900

    Update 00_image.py

--- pages/00_image.py
+++ pages/00_image.py
@@ -1,3 +1,4 @@
 import streamlit as st
 
+st.image("https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExdjA2OXd5bmFiazB5YnRxc3lrNWw4YjNneHY3eTA2azYwZmZlZWZ2NSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/UtoW4GSVDtjQQ/giphy.gif")
 st.image("https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExOWh5emlyMGFrY2pxZjR0ZDA0bnJpNWh6cmo0Y2Zobzg2cTh1Y3FweSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/14d8Aj81ffgnQY/giphy.gif")
